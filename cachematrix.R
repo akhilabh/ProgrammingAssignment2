42575d1cb1e52261ddedcf48e681f627802a16d0
Akhila B <akhilab@uw.edu>
2018-07-24 11:18:56 -0700

    Update cachematrix.R
    new edits

--- cachematrix.R
+++ cachematrix.R
@@ -1,27 +1,31 @@
- 
- makeCacheMatrix <- function(x = matrix()) {
-       
-         set <- function(y) {
-                 x <<- y
-                 Inverses <<- NULL
-         }
-         get <- function() x
-         createInverse <- function(inverse) Inverses <<- inverse
-         pullInverse <- function()Inversesinv
-         list(
-              createInverse = createInverse,
-              pullInverse = pullInverse)
- }
+
+rm(list = ls())
+
+#This function creates the inverse of whatever matrix that is passed in, after which it caches it
 
 
- cacheSolve <- function(x) {
-        
-         Inverses <- x$pullInverse()
-        if (!is.null(Inverses)) {
-                 message("getting cached data")
-                 return(Inverses)
-         }
-         x$createInverse(Inverses)
-         Inverses
- }
- 
+makeCacheMatrix <- function(x = matrix()) {
+  matrix <<- solve(x) 
+  matrix
+}
+
+#this verson checks for the validity of the matrix, and then it does the inverse of the function
+
+
+cacheSolve <- function(x,... ) {
+ #checking to see if the matrix exists
+  if(exists('matrix')) {
+    message("Congratulations! the inverse object exists")
+    return(matrix)
+  } 
+ #if it does not exist then the program sends a message to the user that it does not work and then it sends it to the matrix
+ else { 
+    message("this object does not exist :( ")
+    makeCacheMatrix(x)
+}
+}
+
+#I tested the function with the tester function
+
+Tester <- matrix(1:7,3,3)
+cacheSolve(Tester)
